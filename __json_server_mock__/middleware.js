9fe23a2b6db88f09cb723d31a26b8bb1e65b647e
zyq <zyq6227738@163.com>
2021-09-26 18:00:02 +0800

    feat:add login code

---
+++ __json_server_mock__/middleware.js
@@ -0,0 +1,17 @@
+module.exports = (req, res, next) => {
+  console.log("========================");
+  console.log(res.path);
+  if (req.method === "POST" && req.path === "/login") {
+    console.log("-----------------------");
+    if (req.body.username === "jira" && req.body.password === "123") {
+      return res.status(200).json({
+        user: {
+          token: "123",
+        },
+      });
+    } else {
+      return res.status(400).json({ message: "用户名或密码错误" });
+    }
+  }
+  next();
+};
