93ef4c8943b78671a615ee1281de1ad77ae29b85
zyq <zyq6227738@163.com>
2021-10-14 15:29:39 +0800

    feat: add craco & antd

--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Input, Select } from "antd";
 export interface User {
   id: string;
   name: string;
@@ -19,24 +20,22 @@ export const SearchPanel = ({ param, users, setParam }: ISearchPanel) => {
   return (
     <form action="">
       <div>
-        <input
+        <Input
           type="text"
           value={param.name}
           onChange={(evt) => setParam({ ...param, name: evt.target.value })}
         />
-        <select
-          name=""
-          id=""
+        <Select
           value={param.personId}
-          onChange={(evt) => setParam({ ...param, personId: evt.target.value })}
+          onChange={(value) => setParam({ ...param, personId: value })}
         >
           <option value="">负责人</option>
           {users.map((user) => (
-            <option value={user.id} key={user.id}>
+            <Select.Option value={user.id} key={user.id}>
               {user.name}
-            </option>
+            </Select.Option>
           ))}
-        </select>
+        </Select>
       </div>
     </form>
   );
--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,29 +1,45 @@
 import React, { FormEvent } from "react";
 import { useAuth } from "context/auth-context";
+import { Form, Input, Button } from "antd";
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const LoginScreen = () => {
   const { login, user } = useAuth();
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const username = (event.currentTarget.elements[0] as HTMLInputElement)
-      .value;
-    const password = (event.currentTarget.elements[1] as HTMLInputElement)
-      .value;
-    login({ username, password });
+  // const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const username = (event.currentTarget.elements[0] as HTMLInputElement)
+  //     .value;
+  //   const password = (event.currentTarget.elements[1] as HTMLInputElement)
+  //     .value;
+  //   login({ username, password });
+  // };
+  const handleSubmit = (values: { username: string; password: string }) => {
+    login(values);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      {user ? <p>登录成功，用户名：{user?.name} </p> : null}
-      <div>
-        <label htmlFor="username">用户名</label>
-        <input type="text" id="username" />
-      </div>
-      <div>
-        <label htmlFor="password">密码</label>
-        <input type="password" id="password" autoComplete="off" />
-      </div>
-      <button type="submit">登录</button>
-    </form>
+    <Form onFinish={handleSubmit}>
+      <Form.Item
+        name={"username"}
+        rules={[{ required: true, message: "请输入用户名" }]}
+      >
+        <Input placeholder="用户名" type="text" id="username" />
+      </Form.Item>
+      <Form.Item
+        name={"password"}
+        rules={[{ required: true, message: "请输入密码" }]}
+      >
+        <Input
+          placeholder="密码"
+          type="password"
+          id="password"
+          autoComplete="off"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          登录
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
