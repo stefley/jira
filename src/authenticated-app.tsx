34d6465bcebc11823f7ea291258a3de2497f3d5f
zyq <zyq6227738@163.com>
2021-10-15 15:36:38 +0800

    style: 用自定义Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,44 @@
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
 import { useAuth } from "context/auth-context";
+import { Row } from "components/lib";
+import styled from "@emotion/styled";
 
 export const AuthenticateApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  grid-template-areas: "header" "main";
+  height: 100vh;
+`;
+const Header = styled(Row)`
+  grid-area: header;
+  justify-content: space-between;
+`;
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+const Main = styled.main`
+  grid-area: main;
+  height: calc(100vh - 6rem);
+`;
---
+++ src/components/lib.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+type RowProps = {
+  gap?: number | boolean;
+  between?: boolean;
+  marginBottom?: number;
+};
+
+export const Row = styled.div<RowProps>`
+  display: flex;
+  align-items: center;
+  justify-content: ${(props) => (props.between ? "space-between" : undefined)};
+  margin-bottom: ${(props) =>
+    props.marginBottom ? props.marginBottom + "rem" : undefined};
+  > * {
+    margin-top: 0;
+    margin-bottom: 0;
+    margin-right: ${(props) =>
+      typeof props.gap === "number"
+        ? props.gap + "rem"
+        : props.gap
+        ? "2rem"
+        : undefined};
+  }
+`;
